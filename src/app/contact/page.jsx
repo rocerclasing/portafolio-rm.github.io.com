83ba268d1b5116ddf513d63634378310a5d078a2
Robert <robertclasing@gmail.com>
2024-10-30 20:34:13 -0300

    cambior3

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import emailjs from "emailjs-com"; // Make sure to import emailjs
 
-
 const Page = () => {
     const [data, setData] = useState({
         to_name: '',
@@ -40,7 +39,7 @@ const Page = () => {
     };
 
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-gray-900 min-h-screen"> {/* Asegúrate de que el fondo se aplique a toda la pantalla */}
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
