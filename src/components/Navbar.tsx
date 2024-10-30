8d220e59c135c09240ada6c892bae94d97e12049
Robert <robertclasing@gmail.com>
2024-10-29 22:39:53 -0300

    navbar en ingles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav className='bg-gray-800 p-4 shadow-md'>
       <div className='container mx-auto flex items-center justify-between'>
         <div className='flex flex-grow justify-center space-x-8'>
-          <Link href="/" className='text-white hover:text-gray-300 transition duration-200'>Inicio</Link>
+          <Link href="/" className='text-white hover:text-gray-300 transition duration-200'>Home</Link>
           <Link href="/about" className='text-white hover:text-gray-300 transition duration-200'>About</Link>
           <Link href="/projects" className='text-white hover:text-gray-300 transition duration-200'>Projects</Link>
           <Link href="/contact" className='text-white hover:text-gray-300 transition duration-200'>Contact</Link>
